2069b5cda5d013dc6b89f5f48ca1e762410191d4
lotosbin <lotosbin@gmail.com>
2026-01-27 21:52:13 +0800

    Lingering Voices CN

--- bin2_b42/Contents/mods/Lingering Voices CN/42/media/lua/server/LWtalking_patch.lua
+++ bin2_b42/Contents/mods/Lingering Voices CN/42/media/lua/server/LWtalking_patch.lua
@@ -1,117 +1,107 @@
--- Patch 模组：翻译 Lingering Voices 的所有台词
--- 此文件必须在原模组之后加载
+-- Lingering Voices CN Patch
+-- Override global variables with getText() for Chinese translation
 
--- 扑击时的台词
-lungeList = {
-	"躲起来...",
-	"跑...",
-	"救救我们...",
-	"救救我...",
-    "...你",
-    "你！...",
-    "...阻止我！",
-    "...阻止我们！",
-    "...不",
-    "不...！",
-    "哈哈哈...",
-    "哈哈哈哈...",
-    "哈哈...哈...",
-}
+local function patchLingeringVoicesCN()
 
--- 被击中/踉跄时的台词
-staggerList = {
-    "哈哈哈...",
-    "哈哈哈哈...",
-    "哈哈...哈...",
-    "...不",
-    "...不！",
-    "...不-不",
-	"...不-不！",
-    "不...！",
-    "不-不...！",
-	"...天哪",
-    "天哪...",
-    "跑...",
-    "跑！...",
-    "我不行...",
-    "...等等！",
-    "...等等",
-    "...等-等",
-    "等等！...",
-    "等-等！...",
-    "...求求你！",
-    "...求求你",
-	"...求-求你",
-    "求求你！...",
-    "求-求你！...",
-    "...别！",
-    "...别",
-    "...别-别",
-    "别！...",
-    "别-别！...",
-    "...你",
-    "你！...",
-    "...停下",
-    "停下！...",
-}
+    lungeLines = { zombieLine:new("%s", {
+        getText("Sandbox_LW_lunge_1"),
+        getText("Sandbox_LW_lunge_2"),
+        getText("Sandbox_LW_lunge_3"),
+        getText("Sandbox_LW_lunge_4"),
+        getText("Sandbox_LW_lunge_5"),
+        getText("Sandbox_LW_lunge_6"),
+        getText("Sandbox_LW_lunge_7"),
+        getText("Sandbox_LW_lunge_8"),
+        getText("Sandbox_LW_lunge_9"),
+        getText("Sandbox_LW_lunge_10"),
+        getText("Sandbox_LW_lunge_11"),
+        getText("Sandbox_LW_lunge_12"),
+        getText("Sandbox_LW_lunge_13"),
+    })}
 
--- 假死时的台词
-fakeDeadList = {
-	"...不",
-    "...跑",
-    "...等等",
-    "...求求你",
-    "哈哈...哈...",
-    "哈哈哈...",
-    "哈哈哈哈...",
-    "...你",
-}
+    staggerLines = { zombieLine:new("%s", {
+        getText("Sandbox_LW_stagger_1"),
+        getText("Sandbox_LW_stagger_2"),
+        getText("Sandbox_LW_stagger_3"),
+        getText("Sandbox_LW_stagger_4"),
+        getText("Sandbox_LW_stagger_5"),
+        getText("Sandbox_LW_stagger_6"),
+        getText("Sandbox_LW_stagger_7"),
+        getText("Sandbox_LW_stagger_8"),
+        getText("Sandbox_LW_stagger_9"),
+        getText("Sandbox_LW_stagger_10"),
+        getText("Sandbox_LW_stagger_11"),
+        getText("Sandbox_LW_stagger_12"),
+        getText("Sandbox_LW_stagger_13"),
+        getText("Sandbox_LW_stagger_14"),
+        getText("Sandbox_LW_stagger_15"),
+        getText("Sandbox_LW_stagger_16"),
+        getText("Sandbox_LW_stagger_17"),
+        getText("Sandbox_LW_stagger_18"),
+        getText("Sandbox_LW_stagger_19"),
+        getText("Sandbox_LW_stagger_20"),
+        getText("Sandbox_LW_stagger_21"),
+        getText("Sandbox_LW_stagger_22"),
+        getText("Sandbox_LW_stagger_23"),
+        getText("Sandbox_LW_stagger_24"),
+        getText("Sandbox_LW_stagger_25"),
+        getText("Sandbox_LW_stagger_26"),
+        getText("Sandbox_LW_stagger_27"),
+        getText("Sandbox_LW_stagger_28"),
+        getText("Sandbox_LW_stagger_29"),
+        getText("Sandbox_LW_stagger_30"),
+        getText("Sandbox_LW_stagger_31"),
+        getText("Sandbox_LW_stagger_32"),
+    })}
 
--- 敲门/敲击时的台词
-thumpingList = {
-	"喂...？",
-	"...求求你",
-    "...开门！",
-    "开门！...",
-    "...你",
-    "哈哈...哈...",
-    "哈哈哈...",
-    "把它打开！...",
-    "...打开它！",
-    "有人吗...？",
-}
+    fakeDeadLines = { zombieLine:new("%s", {
+        getText("Sandbox_LW_fakeDead_1"),
+        getText("Sandbox_LW_fakeDead_2"),
+        getText("Sandbox_LW_fakeDead_3"),
+        getText("Sandbox_LW_fakeDead_4"),
+        getText("Sandbox_LW_fakeDead_5"),
+        getText("Sandbox_LW_fakeDead_6"),
+        getText("Sandbox_LW_fakeDead_7"),
+        getText("Sandbox_LW_fakeDead_8"),
+    })}
 
--- 攻击时的台词
-attackList = {
-	"对不起...",
-	"...我...不行",
-	"...求求你别...",
-	"...停-停下我",
-    "...停下我！",
-	"...停下我们",
-	"...不...不",
-	"...别再这样了",
-    "...救命",
-    "救救我们...",
-	"救救我...",
-    "跑...",
-    "...有人吗！",
-    "...任何人！",
-    "离远点...",
-    "保持清醒...",
-    "你！...",
-    "...你",
-    "...等等",
-    "...等-等",
-}
-local function handleCustom(linesTable, customList)
-    if  #customList ~= 0 then
-        table.insert( linesTable, zombieLine:new("%s", customList))
-    end
+    thumpingLines = { zombieLine:new("%s", {
+        getText("Sandbox_LW_thumping_1"),
+        getText("Sandbox_LW_thumping_2"),
+        getText("Sandbox_LW_thumping_3"),
+        getText("Sandbox_LW_thumping_4"),
+        getText("Sandbox_LW_thumping_5"),
+        getText("Sandbox_LW_thumping_6"),
+        getText("Sandbox_LW_thumping_7"),
+        getText("Sandbox_LW_thumping_8"),
+        getText("Sandbox_LW_thumping_9"),
+        getText("Sandbox_LW_thumping_10"),
+    })}
+
+    attackLines = { zombieLine:new("%s", {
+        getText("Sandbox_LW_attack_1"),
+        getText("Sandbox_LW_attack_2"),
+        getText("Sandbox_LW_attack_3"),
+        getText("Sandbox_LW_attack_4"),
+        getText("Sandbox_LW_attack_5"),
+        getText("Sandbox_LW_attack_6"),
+        getText("Sandbox_LW_attack_7"),
+        getText("Sandbox_LW_attack_8"),
+        getText("Sandbox_LW_attack_9"),
+        getText("Sandbox_LW_attack_10"),
+        getText("Sandbox_LW_attack_11"),
+        getText("Sandbox_LW_attack_12"),
+        getText("Sandbox_LW_attack_13"),
+        getText("Sandbox_LW_attack_14"),
+        getText("Sandbox_LW_attack_15"),
+        getText("Sandbox_LW_attack_16"),
+        getText("Sandbox_LW_attack_17"),
+        getText("Sandbox_LW_attack_18"),
+        getText("Sandbox_LW_attack_19"),
+        getText("Sandbox_LW_attack_20"),
+    })}
+
+    print("[Lingering Voices CN] Chinese patch loaded!")
 end
---handleCustom(idleLines,zombieLine:new("%s", IdleList))
-handleCustom(lungeLines,zombieLine:new("%s", lungeList))
-handleCustom(staggerLines,zombieLine:new("%s", staggerList))
-handleCustom(fakeDeadLines, zombieLine:new("%s", fakeDeadList))
-handleCustom(thumpingLines, zombieLine:new("%s", thumpingList))
-handleCustom(attackLines, zombieLine:new("%s", attackList) )
+
+patchLingeringVoicesCN()
